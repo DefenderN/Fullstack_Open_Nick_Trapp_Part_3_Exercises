b635f9206ea96f5f16c08b29543cc2ec77673464
Nick Surface <nick.trapp@gmx.de>
2024-03-18 11:38:20 +0100

    Installed Express for backend development. Finished exercise 3.1: Implementing a GET method that returns a hardcoded string of the persons array on ./api/persons

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("Hello world")
+//import Express library
+const express = require('express')
+// create express application and store it in the app variable
+const app = express()
+
+// raw server data (for the time being)
+let notes = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+//return hardcoded string of persons
+app.get('/api/persons', (request, response) => {
+  response.json(notes)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
