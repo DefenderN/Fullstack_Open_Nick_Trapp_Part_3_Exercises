56a96472db011e6d379ead38dea3e8fec633855f
Nick Surface <nick.trapp@gmx.de>
2024-03-19 12:19:22 +0100

    Finished exercise 3.6: Phonebook backend step 6

--- index.js
+++ index.js
@@ -73,25 +73,37 @@ app.delete("/api/persons/:id", (request, response) => {
 //Add a single person
 app.post("/api/persons", (request, response) => {
 
+  //Assign input data easy to read variable names
   let name = request.body.name
   let number = request.body.number
   let id = Math.floor(Math.random() * 10000) + 1;
 
-  let newPerson = {
-    "id": id,
-    "name": name,
-    "number": number,
-  }
   //check that the provided name and number are truthy
-  if (name && number){
-    let newArrayLength = persons.push(newPerson)
-    
-    // Assuming successful processing
-    // Respond with a 200 OK status code and a JSON object indicating success
-    response.status(200).json({ message: 'Data received successfully', addedPerson: newPerson });
+  //return 400 and an error message if that is not the case
+  if (!name || !number) {
+    response.status(400).json({error: 'Invalid request. Check your request data and try again.'});
   }
+
+  //check that the name is not already in the phonebook.
+  //In that case return an error message aswell, explaining 
+  //that the name already exists
+  else if (persons.some(person => person.name === name)) {
+    response.status(400).json({error: 'The name is already in the phonebook'});
+  }
+
   else {
-    response.status(400).json({ error: 'Invalid request. Check your request data and try again.' });
+    console.log("Last statement is called")
+    //Create new person object
+    let newPerson = {
+      "id": id,
+      "name": name,
+      "number": number,
+    }
+    //Add new person
+    persons.push(newPerson);
+    // Respond with 200 OK status code and a success message and
+    // the data of the added person
+    response.status(200).json({message: 'Data received successfully', addedPerson: newPerson});
   }
 })
 
