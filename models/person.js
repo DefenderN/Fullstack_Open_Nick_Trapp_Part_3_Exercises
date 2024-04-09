2660ef96339c409aa2dfca6203ff3a36f4025588
Nick Surface <nick.trapp@gmx.de>
2024-04-09 14:48:48 +0200

    used eslint 1st time but i had to revert changes to the dist folder

--- models/person.js
+++ models/person.js
@@ -1,44 +1,44 @@
 // import mongoose
-const mongoose = require(`mongoose`)
+const mongoose = require('mongoose')
 
 // setup mongoose
-mongoose.set(`strictQuery`, false)
+mongoose.set('strictQuery', false)
 
 // URI of MongoDB
 const url = process.env.MONGODB_URI
 
 // logging
-console.log(`connecting to`, url)
+console.log('connecting to', url)
 
 // Connect to DB
 mongoose.connect(url)
-    .then(result => {
-        console.log(`Connected to MongoDB`)
-    })
-    .catch(error => {
-        console.log(`error connecting to MongoDB:`, error.message)
-    })
+  .then(result => {
+    console.log('Connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 // Define validator functions for person Schema
 const validatePhoneNumber = (phoneNumber) => {
-    const regEx = /^\d{2,3}-\d{1,}$/;
-    return regEx.test(phoneNumber)
+  const regEx = /^\d{2,3}-\d{1,}$/
+  return regEx.test(phoneNumber)
 }
 
 // Define person Schema
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: [3, `Name must be a minimum of 3 letters`]
-    },
-    number: {
-        type: String,
-        minLength: [8, `Number must be a minimum of 8 characters`],
-        validate: {
-            validator: validatePhoneNumber,
-            message: "The number has to be of format 123-1234... or 12-1234...."
-        }
+  name: {
+    type: String,
+    minLength: [3, 'Name must be a minimum of 3 letters']
+  },
+  number: {
+    type: String,
+    minLength: [8, 'Number must be a minimum of 8 characters'],
+    validate: {
+      validator: validatePhoneNumber,
+      message: 'The number has to be of format 123-1234... or 12-1234....'
     }
+  }
 })
 
 // Use the transform function to modify the toJSON option of Mongoose.
@@ -47,15 +47,15 @@ const personSchema = new mongoose.Schema({
 // Note: It only modifies the object that is passed to the toJSON function
 // It DOES NOT modify the DB entry at all!
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 // Create fancy Constructor for the person(s) using the personSchema
-//The first argument is the name of the collection (in singular) and the second argument seems to be the schema.
+// The first argument is the name of the collection (in singular) and the second argument seems to be the schema.
 // (to ensure that it throws an error or something if you use a wrong schema in your application)
 // and
 // Export this constructor to use outside of this module.
--- mongo.js
+++ mongo.js
@@ -2,28 +2,28 @@
 const mongoose = require('mongoose')
 
 // Check for password provided by user
-if (process.argv.length<3) {
+if (process.argv.length < 3) {
   console.log('give password as a third argument: e.g. node mongo.js <yourPassword>')
   process.exit(1)
 }
 
-//Assign the password to a variable
+// Assign the password to a variable
 const password = process.argv[2]
 
-//hardcoded url path for our database
+// hardcoded url path for our database
 const url =
   `mongodb+srv://fullstackuser:${password}@fullstackopendb1.qre0tv4.mongodb.net/?retryWrites=true&w=majority&appName=fullstackopendb1`
 
-//setup mongoose (?)
-mongoose.set('strictQuery',false)
+// setup mongoose (?)
+mongoose.set('strictQuery', false)
 
-//connect to db 
+// connect to db
 mongoose.connect(url)
 
-//Create personSchema, which tells mongoDB how to store person objects in the DB
+// Create personSchema, which tells mongoDB how to store person objects in the DB
 const personSchema = new mongoose.Schema({
   name: String,
-  number: String,
+  number: String
 })
 
 // Use the transform function to modify the toJSON option of Mongoose.
@@ -40,34 +40,31 @@ personSchema.set('toJSON', {
 })
 
 // Create fancy Constructor for the person(s) using the personSchema
-//The first argument is the name of the collection (in singular) and the second argument seems to be the schema.
+// The first argument is the name of the collection (in singular) and the second argument seems to be the schema.
 // (to ensure that it throws an error or something if you use a wrong schema in your application)
 const Person = mongoose.model('Person', personSchema)
 
-
-if (process.argv.length<4) {
-  //return all entries of the collection in the db for persons
+if (process.argv.length < 4) {
+  // return all entries of the collection in the db for persons
   Person.find({}).then(result => {
     result.forEach(person => {
       console.log(person)
     })
-  mongoose.connection.close()
+    mongoose.connection.close()
   })
-}
-else {
+} else {
   // Create a person object
   const person1 = new Person({
     name: process.argv[3],
-    number: process.argv[4],
+    number: process.argv[4]
   })
 
   // save the person to the collection in the db on the server
   // By mongoose convention the collection name is the plural of its documents aka person entries:
   // DB Name: Persons
   person1.save().then(result => {
-      console.log('Person saved!')
-    //IMPORTANT to close connection, otherwise it runs indefinetly
+    console.log('Person saved!')
+    // IMPORTANT to close connection, otherwise it runs indefinetly
     mongoose.connection.close()
   })
 }
-
